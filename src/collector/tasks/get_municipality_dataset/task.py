e3b17d02ed5dee8400a08a4b211299d6cde4f4a6
tomdewildt <t.l.dewildt@gmail.com>
2020-04-07 00:07:35 +0200

    Add get municipality dataset task

---
+++ src/collector/tasks/get_municipality_dataset/task.py
@@ -0,0 +1,59 @@
+import logging
+import json
+import io
+
+from bs4 import BeautifulSoup
+import pandas as pd
+
+from collector.schema import obj, string, validate
+from collector.utils import format_date
+
+log = logging.getLogger(__name__)
+
+
+class GetMunicipalityDatasetError(Exception):
+    pass
+
+
+class GetMunicipalityDataset:
+    inputs_schema = obj(output_folder=string())
+
+    def __init__(self, config, client, store):
+        self._config = config
+        self._client = client
+        self._store = store
+
+    def __call__(self, **kwargs):
+        validate(self.inputs_schema, kwargs)
+        return self.run(kwargs)
+
+    def run(self, inputs):
+        log.info("Requesting document")
+        document = self._client.get(self._config["url"])
+
+        log.info("Parsing document")
+        soup = BeautifulSoup(document, features="html.parser")
+        data_element = soup.find(id=self._config["elements"]["data"])
+        metadata_element = soup.find(id=self._config["elements"]["metadata"])
+        if data_element is None:
+            raise GetMunicipalityDatasetError("Data element not found in document")
+        if metadata_element is None:
+            raise GetMunicipalityDatasetError("Metadata element not found in document")
+
+        log.info("Parsing data")
+        data = pd.read_csv(io.StringIO(data_element.text), delimiter=";", decimal=",")
+
+        metadata = metadata_element.text
+        metadata = json.loads(metadata)
+
+        log.info("Storing dataset")
+        path = f"{inputs['output_folder']}/{format_date(metadata)}.csv"
+
+        self._write(data, path, index=False)
+
+    def _write(self, data, path, **kwargs):
+        with self._store.open(path, "w") as handle:
+            data.to_csv(handle, **kwargs)
+
+    def __repr__(self):
+        return "<{}()>".format(self.__class__.__name__)
---
+++ test/test_collector/test_tasks/test_get_municipality_dataset/test_get_municipality_dataset.py
@@ -0,0 +1,102 @@
+import pandas as pd
+import numpy as np
+import pytest
+import mock
+
+from collector.tasks.get_municipality_dataset.task import (
+    GetMunicipalityDataset,
+    GetMunicipalityDatasetError,
+)
+from collector.schema import ValidationError
+from data import create_config, create_response
+from fixtures import Client, Store
+
+
+class TestGetMunicipalityDatasetRun:
+    @property
+    def config(self):
+        return create_config()
+
+    @mock.patch.object(GetMunicipalityDataset, "run")
+    def test_run_valid_input(self, mock_run):
+        task = GetMunicipalityDataset(self.config["collector"], Client(), Store())
+        task(output_folder="raw")
+
+        mock_run.assert_called_once_with({"output_folder": "raw"})
+
+    @pytest.mark.parametrize(
+        "inputs,messages",
+        [
+            ({}, ["'output_folder' is a required property"]),
+            ({"output_folder": 1}, ["1 is not of type 'string'"]),
+            ({"output-folder": 1}, ["'output_folder' is a required property"]),
+        ],
+    )
+    @mock.patch.object(GetMunicipalityDataset, "run")
+    def test_run_invalid_input(self, mock_run, inputs, messages):
+        task = GetMunicipalityDataset(self.config["collector"], Client(), Store())
+        with pytest.raises(ValidationError) as error:
+            task(**inputs)
+
+        mock_run.assert_not_called()
+
+        for (idx, error) in enumerate(error.value.errors):
+            assert error.message == messages[idx]
+
+    @mock.patch.object(Client, "get")
+    def test_run_invalid_data_element(self, mock_get):
+        mock_get.return_value = "<div id='metadata'></div>"
+
+        task = GetMunicipalityDataset(self.config["collector"], Client(), Store())
+        with pytest.raises(GetMunicipalityDatasetError) as error:
+            task(output_folder="raw")
+
+        assert str(error.value) == "Data element not found in document"
+
+    @mock.patch.object(Client, "get")
+    def test_run_invalid_metadata_element(self, mock_get):
+        mock_get.return_value = "<div id='data'></div>"
+
+        task = GetMunicipalityDataset(self.config["collector"], Client(), Store())
+        with pytest.raises(GetMunicipalityDatasetError) as error:
+            task(output_folder="raw")
+
+        assert str(error.value) == "Metadata element not found in document"
+
+    @mock.patch.object(Client, "get")
+    @mock.patch.object(GetMunicipalityDataset, "_write")
+    def test_run(self, mock_write, mock_get):
+        mock_get.return_value = create_response(
+            data="id;gemeente;aantal\n1;gemeente 1;1\n2;gemeente 2;2\n",
+            metadata="map subtitle 1-1-1970",
+        )
+
+        task = GetMunicipalityDataset(self.config["collector"], Client(), Store())
+        task(output_folder="raw")
+
+        mock_get.assert_called_once_with(self.config["collector"]["url"])
+        mock_write.assert_called_once_with(mock.ANY, "raw/1970-01-01.csv", index=False)
+
+        pd.testing.assert_frame_equal(
+            mock_write.call_args.args[0],
+            pd.DataFrame(
+                {
+                    "id": [1, 2],
+                    "gemeente": ["gemeente 1", "gemeente 2"],
+                    "aantal": [1, 2],
+                }
+            ),
+            check_dtype=False,
+        )
+
+
+class TestGetMunicipalityDatasetWrite:
+    @mock.patch.object(Store, "open")
+    @mock.patch.object(pd.DataFrame, "to_csv")
+    def test_write(self, mock_to_csv, mock_open):
+        # pylint: disable=protected-access
+        task = GetMunicipalityDataset(None, Client(), Store())
+        task._write(pd.DataFrame(np.zeros(shape=(3, 3))), "test.csv", index=False)
+
+        mock_to_csv.assert_called_once_with(mock.ANY, index=False)
+        mock_open.assert_called_once_with("test.csv", "w")
