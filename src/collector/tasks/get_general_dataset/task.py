370f183d647f6495b2164a0f7b36c046ab151dfc
tomdewildt <t.l.dewildt@gmail.com>
2020-04-07 00:07:07 +0200

    Add get general dataset task

---
+++ src/collector/tasks/get_general_dataset/task.py
@@ -0,0 +1,63 @@
+import logging
+import json
+
+from bs4 import BeautifulSoup
+import pandas as pd
+
+from collector.schema import obj, string, validate
+from collector.utils import format_date
+
+log = logging.getLogger(__name__)
+
+
+class GetGeneralDatasetError(Exception):
+    pass
+
+
+class GetGeneralDataset:
+    inputs_schema = obj(output_folder=string())
+
+    def __init__(self, config, client, store):
+        self._config = config
+        self._client = client
+        self._store = store
+
+    def __call__(self, **kwargs):
+        validate(self.inputs_schema, kwargs)
+        return self.run(kwargs)
+
+    def run(self, inputs):
+        log.info("Requesting document")
+        document = self._client.get(self._config["url"])
+
+        log.info("Parsing document")
+        soup = BeautifulSoup(document, features="html.parser")
+        data_element = soup.find(self._config["elements"]["general"])
+        metadata_element = soup.find(id=self._config["elements"]["metadata"])
+        if data_element is None:
+            raise GetGeneralDatasetError("Data element not found in document")
+        if metadata_element is None:
+            raise GetGeneralDatasetError("Metadata element not found in document")
+
+        log.info("Parsing data")
+        data = pd.DataFrame(columns=["PositiefGetest", "Opgenomen", "Overleden"])
+        for (idx, row) in enumerate(data_element.findAll("tr")):
+            element = row.findAll("td")[1].find("h4")
+            amount = element.text.split()[0].replace(".", "").replace("*", "")
+
+            data.at[0, data.columns[idx]] = int(amount)
+
+        metadata = metadata_element.text
+        metadata = json.loads(metadata)
+
+        log.info("Storing dataset")
+        path = f"{inputs['output_folder']}/{format_date(metadata)}.csv"
+
+        self._write(data, path, index=False)
+
+    def _write(self, data, path, **kwargs):
+        with self._store.open(path, "w") as handle:
+            data.to_csv(handle, **kwargs)
+
+    def __repr__(self):
+        return "<{}()>".format(self.__class__.__name__)
