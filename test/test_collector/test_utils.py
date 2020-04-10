8e48eb44c0325eaa3b23e023c3750282bd03cee4
tomdewildt <t.l.dewildt@gmail.com>
2020-04-11 01:29:12 +0200

    Add filter files function to utils

--- test/test_collector/test_utils.py
+++ test/test_collector/test_utils.py
@@ -1,6 +1,6 @@
 import pytest
 
-from collector.utils import format_date
+from collector.utils import format_date, filter_files
 
 
 class TestFormatDate:
@@ -13,3 +13,33 @@ class TestFormatDate:
     )
     def test_format_date(self, metadata, date):
         assert format_date(metadata) == date
+
+
+class TestFilterFiles:
+    @pytest.mark.parametrize(
+        "files,output",
+        [
+            ([], []),
+            (["/tmp/1970-01-01-file-1.csv"], ["/tmp/1970-01-01-file-1.csv"]),
+            (
+                [
+                    "/tmp/1970-01-01-file-1.csv",
+                    "/tmp/1970-01-02-file-1.csv",
+                    "/tmp/1970-01-03-file-1.csv",
+                    "/tmp/1970-01-04-file-1.csv",
+                ],
+                ["/tmp/1970-01-04-file-1.csv"],
+            ),
+            (
+                [
+                    "/tmp/1969-01-01-file-1.csv",
+                    "/tmp/1969-01-01-file-2.csv",
+                    "/tmp/1970-01-01-file-1.csv",
+                    "/tmp/1970-01-01-file-2.csv",
+                ],
+                ["/tmp/1970-01-01-file-1.csv", "/tmp/1970-01-01-file-2.csv"],
+            ),
+        ],
+    )
+    def test_format_date(self, files, output):
+        assert filter_files(files) == output
